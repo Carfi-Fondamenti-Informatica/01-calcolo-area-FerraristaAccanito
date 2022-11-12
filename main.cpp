5449a34c680dc984992bc2609c1086ab150d88d8
FerraristaAccanito <117997140+FerraristaAccanito@users.noreply.github.com>
2022-11-12 10:00:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,27 @@
 using namespace std;
 
 int main(){
+
 //Qui vanno definite le variabili
 
-cin >> a >> b >> c;
+    float a = 0;
+    float b = 0;
+    float c = 0;
+
+    cout << "inserisci tre numeri\n";
+    cin >> a >> b >> c;
+
+    float AreaTriangolo = (a*b) / 2 ;
+    float AreaQuadrato = a*a ;
+    float AreaRettangolo = a*b ;
+    float AreaTrapezio = (a+b)*c / 2 ;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
 
+    cout << "AREA TRIANGOLO :  \n" << AreaTriangolo;
+    cout << "AREA QUADRATO : \n" << AreaQuadrato;
+    cout << "AREA RETTANGOLO :  \n" << AreaRettangolo;
+    cout << "AREA TRAPEZIO :  \n" << AreaTrapezio;
 
+    return 0;
 }
